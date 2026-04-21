002692e81946df9a6919d0af93cee2ee87e8942e
Matias <matiasngallego@gmail.com>
2026-04-20 21:07:45 -0300

    Auth with Github

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,3 @@
+import { handlers } from "@/auth"; // Asegurate de que esta ruta apunte bien a tu auth.ts (puede ser "../../auth" dependiendo de dónde lo pusiste)
+
+export const { GET, POST } = handlers;
